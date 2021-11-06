56125de15d4856b6400d9c91195a25fab8297db2
dexterknaack <dexter.knaack@gmail.com>
2021-11-06 15:12:59 -0700

    Game Controller Death Screen

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,21 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public static Transform player;
     [SerializeField] private Transform playerRef;
+
+    
+    [SerializeField] private TextMeshProUGUI[] deathUI;
+    [SerializeField] private Image fadeImg;
+    private bool fading = false;
+    private bool fadingIn = false;
+    [SerializeField] private float fadeRate;
+
     // Start is called before the first frame update
     void Awake()
     {
         player = playerRef;
-        //player = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (TextMeshProUGUI t in deathUI)
+        {
+            t.color = new Color(t.color.r, t.color.g, t.color.b, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (fadingIn)
+        {
+            if (fadeImg.color.a >= 1)
+            {
+                Debug.Log("hi");
+                fadingIn = false;
+            }
+            if (fadeImg.color.a >= .70)
+            {
+                FadeTextIn();
+            }
+            fadeImg.color = new Color(fadeImg.color.r, fadeImg.color.g, fadeImg.color.b, fadeImg.color.a + fadeRate * Time.deltaTime);
+        }
+        if (fading)
+        {
+            foreach (TextMeshProUGUI t in deathUI)
+            {
+                if (t.color.a >= 1)
+                {
+                    fading = false;
+                }
+                t.color = new Color(t.color.r, t.color.g, t.color.b, t.color.a + fadeRate * Time.deltaTime);
+            }
+        }
+    }
+
+    public void LoadLevel(int val)
+    {
+        SceneManager.LoadScene(val);
+    }
+
+    public void Die()
+    {
+
+        fadingIn = true;
+    }
+    private void FadeTextIn()
+    {
+        fading = true;
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     private PlayerMove playerMove;
+    private Rigidbody2D rb;
+    [SerializeField] private GameController gameController;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         playerMove = GetComponent<PlayerMove>();
     }
 
@@ -20,5 +23,7 @@ public class PlayerHealth : MonoBehaviour
     public void Kill()
     {
         playerMove.enabled = false;
+        rb.velocity = new Vector2(0, 0);
+        gameController.Die();
     }
 }
